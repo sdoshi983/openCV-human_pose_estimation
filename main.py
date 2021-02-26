352e44b2c85e7681099c55684d3bc9efa62527e4
sdoshi983 <sdoshi983@rku.ac.in>
2021-02-26 23:36:18 +0530

    List of body parts added

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
 import cv2
 import matplotlib.pyplot as plt
+
+net = cv2.dnn.readNetFromTensorflow('assets/graph_opt.pb')
+
+inWidth = 368
+inHeight = 368
+thr = 0.2
+
+BODY_PARTS = { "Nose": 0, "Neck": 1, "RShoulder": 2, "RElbow": 3, "RWrist": 4,
+                   "LShoulder": 5, "LElbow": 6, "LWrist": 7, "RHip": 8, "RKnee": 9,
+                   "RAnkle": 10, "LHip": 11, "LKnee": 12, "LAnkle": 13, "REye": 14,
+                   "LEye": 15, "REar": 16, "LEar": 17, "Background": 18 }
