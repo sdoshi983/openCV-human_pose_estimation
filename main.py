5a9bf532b133f98779eee01e609b8f8ccb5f5ee3
sdoshi983 <sdoshi983@rku.ac.in>
2021-03-02 19:20:13 +0530

    Points list for all the different body parts created

--- main.py
+++ main.py
@@ -25,3 +25,10 @@ def poseEstimation(frame):
     frameWidth = frame.shape[1]
     frameHeight = frame.shape[0]
     net.setInput(cv2.dnn.blobFromImage(frame, 1.0, (inWidth, inHeight), (127.5, 127.5, 127.5), swapRB=True, crop=False))
+
+    out = net.forward()
+    out = out[:, :19, :, :]  # MobileNet output [1, 57, -1, -1], we only need the first 19 elements
+
+    assert (len(BODY_PARTS) == out.shape[1])
+
+    points = []
